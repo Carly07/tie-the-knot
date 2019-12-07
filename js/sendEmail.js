3615c4f5b84e1d58325f4c659438d5b9f7397034
Carly Clark <carly.simpson07@hotmail.co.uk>
2019-12-07 09:23:04 +0000

    fix bugs

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,18 +1,15 @@
-(function() {
-            emailjs.init("user_cAhgSczcxi8hSCfeCwkQf");
-        })();
-
 function sendMail(contactForm) {
-    emailjs.send("outlook", "tie_the_knot", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "enquiry_message": contactForm.message.value,
-    })
-    .then(
-        function (response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
+    emailjs.send("gmail", "tie_the_knot", {
+            "from_name": "from_name_value",
+            "from_email": "from_email_value",
+            "enquiry_subject": "enquiry_subject_value",
+            "enquiry_message": "enquiry_message_value",
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            });
 }
