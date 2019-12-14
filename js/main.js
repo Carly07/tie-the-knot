0ceaeb5c656086308e58f045b47a0166398fb939
Carly Clark <carly.simpson07@hotmail.co.uk>
2019-12-14 16:27:01 +0000

    add infoWindow when marker clicked

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@ function initMap() {
         content: document.getElementById('info-content')
     });
 
+    places = new google.maps.places.PlacesService(map);
+
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
     var searchBox = new google.maps.places.SearchBox(input);
@@ -45,13 +47,14 @@ function initMap() {
 
         // Clear out the old markers.
         markers.forEach(function(marker) {
+            google.maps.event.clearListeners(marker, 'click');
             marker.setMap(null);
         });
-
         markers = [];
 
         // For each place, get the icon, name and location.
         var bounds = new google.maps.LatLngBounds();
+        var count = 0;
         places.forEach(function(place) {
             if (!place.geometry) {
                 console.log("Returned place contains no geometry");
@@ -65,6 +68,7 @@ function initMap() {
                 scaledSize: new google.maps.Size(25, 25)
             };
 
+
             // Create a marker for each place.
             markers.push(new google.maps.Marker({
                 map: map,
@@ -73,6 +77,10 @@ function initMap() {
                 position: place.geometry.location
             }));
 
+            markers[count].placeResult = place;
+
+            google.maps.event.addListener(markers[count], 'click', showInfoWindow);
+
             if (place.geometry.viewport) {
                 // Only geocodes have viewport.
                 bounds.union(place.geometry.viewport);
@@ -80,11 +88,74 @@ function initMap() {
             else {
                 bounds.extend(place.geometry.location);
             }
+
+            count++;
         });
         map.fitBounds(bounds);
     });
+}
 
-    markers.addListener('click', function() {
-        infoWindow.open(map, markers);
-    });
+function showInfoWindow() {
+    var marker = this;
+    places.getDetails({ placeId: marker.placeResult.place_id },
+        function(place, status) {
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                return;
+            }
+            infoWindow.open(map, marker);
+            buildIWContent(place);
+        });
+}
+
+function buildIWContent(place) {
+    document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
+        'src="' + place.icon + '"/>';
+    document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
+        '">' + place.name + '</a></b>';
+    document.getElementById('iw-address').textContent = place.vicinity;
+
+    if (place.formatted_phone_number) {
+        document.getElementById('iw-phone-row').style.display = '';
+        document.getElementById('iw-phone').textContent =
+            place.formatted_phone_number;
+    }
+    else {
+        document.getElementById('iw-phone-row').style.display = 'none';
+    }
+
+    // Assign a five-star rating to the hotel, using a black star ('&#10029;')
+    // to indicate the rating the hotel has earned, and a white star ('&#10025;')
+    // for the rating points not achieved.
+    if (place.rating) {
+        var ratingHtml = '';
+        for (var i = 0; i < 5; i++) {
+            if (place.rating < (i + 0.5)) {
+                ratingHtml += '&#10025;';
+            }
+            else {
+                ratingHtml += '&#10029;';
+            }
+            document.getElementById('iw-rating-row').style.display = '';
+            document.getElementById('iw-rating').innerHTML = ratingHtml;
+        }
+    }
+    else {
+        document.getElementById('iw-rating-row').style.display = 'none';
+    }
+
+    // The regexp isolates the first part of the URL (domain plus subdomain)
+    // to give a short URL for displaying in the info window.
+    if (place.website) {
+        var fullUrl = place.website;
+        var website = hostnameRegexp.exec(place.website);
+        if (website === null) {
+            website = 'http://' + place.website + '/';
+            fullUrl = website;
+        }
+        document.getElementById('iw-website-row').style.display = '';
+        document.getElementById('iw-website').textContent = website;
+    }
+    else {
+        document.getElementById('iw-website-row').style.display = 'none';
+    }
 }
