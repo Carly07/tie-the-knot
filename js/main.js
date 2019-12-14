7ffb9e3214c9ec41448f3c3539dc3972d840a347
Carly Clark <carly.simpson07@hotmail.co.uk>
2019-12-14 15:16:25 +0000

    change supplier dropdown to search box

--- js/main.js
+++ js/main.js
@@ -1,9 +1,90 @@
 $('body').scrollspy({ target: '#navbarScroll', offset: 50 });
 
-var map;
+var map, places, infoWindow;
+
+var countries = {
+    'uk': {
+        center: { lat: 54.8812882, lng: -3.6730459 },
+        zoom: 5
+    }
+};
 
 function initMap() {
-    var uk = { lat: 54.8812882, lng: -3.6730459 };
-    map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 5, center: uk });
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: countries['uk'].zoom,
+        center: countries['uk'].center,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: false,
+        streetViewControl: false
+    });
+
+    infoWindow = new google.maps.InfoWindow({
+        content: document.getElementById('info-content')
+    });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            }
+            else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+
+    markers.addListener('click', function() {
+        infoWindow.open(map, markers);
+    });
 }
